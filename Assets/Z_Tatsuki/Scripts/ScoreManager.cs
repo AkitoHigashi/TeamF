ae4abe2402ec49d1e9006e849f590e8855dbbba2
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 12:17:07 +0900

    [add] 荷物スコアの加算処理と現在スコア取得機能を追加

--- Assets/Z_Tatsuki/Scripts/ScoreManager.cs
+++ Assets/Z_Tatsuki/Scripts/ScoreManager.cs
@@ -3,5 +3,18 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] int Score;
+    
+    //荷物スコアを加算
+    public void AddScore(int luggagescore)
+    {
+        Score += luggagescore;
+        Debug.Log("スコア加算" + Score);
+    }
+    
+    //現在のスコアを取得
+    public int GetScore()
+    {
+        return Score;
+    }
    
 }
