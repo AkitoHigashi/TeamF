c3af8d8e478d52125d0d55a5e943e482fc2792a8
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 13:55:27 +0900

    [clean]使わないhp変数削除

--- Assets/Z_Tatsuki/Scripts/Luggage.cs
+++ Assets/Z_Tatsuki/Scripts/Luggage.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Luggage : MonoBehaviour
 {
     [SerializeField] int _luggagescore;　//荷物のスコア
-    private int _luggagehp;              //荷物HP
     private bool _isDelivered = false; //配達できたかどうか
 
 
