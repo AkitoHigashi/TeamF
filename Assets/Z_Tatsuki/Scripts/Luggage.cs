9b3c6ec4747550a1b6b9470d85604007896c7354
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 11:51:23 +0900

    [clean] 使っていないコードを削除

--- Assets/Z_Tatsuki/Scripts/Luggage.cs
+++ Assets/Z_Tatsuki/Scripts/Luggage.cs
@@ -6,13 +6,13 @@ public class Luggage : MonoBehaviour
 {
     private int  _score;
     private int _luggagehp;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
