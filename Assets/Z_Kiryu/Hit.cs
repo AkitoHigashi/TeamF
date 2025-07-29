10aeaa7b4e8ce17376cd13552b8ab7915587b7a2
KaboKiryu <ki28ryu@icloud.com>
2025-07-29 20:21:28 +0900

    [add]タグを追加

--- Assets/Z_Kiryu/Hit.cs
+++ Assets/Z_Kiryu/Hit.cs
@@ -13,7 +13,7 @@ public class Hit : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("NeedleTrap"))
         {
             Debug.Log("敵と衝突した");
             //ノックバック方向の計算
