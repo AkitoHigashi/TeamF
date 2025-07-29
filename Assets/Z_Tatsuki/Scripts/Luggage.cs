8a6b9f0aaf1faf58ef62a2083f82a51645bf7379
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 11:49:56 +0900

    [add]スコア、HPの変数を追加

--- Assets/Z_Tatsuki/Scripts/Luggage.cs
+++ Assets/Z_Tatsuki/Scripts/Luggage.cs
@@ -1,7 +1,11 @@
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Luggage : MonoBehaviour
 {
+    private int  _score;
+    private int _luggagehp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
