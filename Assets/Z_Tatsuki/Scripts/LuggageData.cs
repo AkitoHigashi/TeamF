a55fc0be121b68fe3ea9af6a2e18c4a055466a95
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 15:33:37 +0900

    [add]ゲームクリアを管理するクラスを作成

--- Assets/Z_Tatsuki/Scripts/LuggageData.cs
+++ Assets/Z_Tatsuki/Scripts/LuggageData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="NewLuggageDatabase",menuName="Luggage Database")]
 public class LuggageData : ScriptableObject
 {
-    public string displayName; //荷物の名前
+    public string DisplayName; //荷物の名前
     public int LuggageScore;　//荷物のスコア
     
 }
--- Assets/Z_Tatsuki/Scripts/ScoreManager.cs
+++ Assets/Z_Tatsuki/Scripts/ScoreManager.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] int Score; //スコアの合計
-
-
+    [SerializeField] int ClearScore; //クリアするための最低限スコア
+   
     #region スコア処理
     public void AddScore(int luggagescore)
     {
         Score += luggagescore;
         Debug.Log("スコア加算" + Score);
+        if (Score > ClearScore)
+        {
+
+        }
     }
 
     //現在のスコアを取得
