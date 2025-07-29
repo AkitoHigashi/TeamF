84ea48cb42a3def85e6dd0f5f17b86e5519a9fa6
kenkatou354 <kenkatou354@gmail.com>
2025-07-29 13:16:00 +0900

    [add]荷物が配達場所に触れたらスコア加算＆削除

--- Assets/Z_Tatsuki/Scripts/Luggage.cs
+++ Assets/Z_Tatsuki/Scripts/Luggage.cs
@@ -1,11 +1,33 @@
-using UnityEditor.Rendering;
+
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+
 
 public class Luggage : MonoBehaviour
 {
-    [SerializeField] int  _luggagescore;　//荷物のスコア
+    [SerializeField] int _luggagescore;　//荷物のスコア
     private int _luggagehp;              //荷物HP
     private bool _isDelivered = false; //配達できたかどうか
+
+    //配達場所においたかどうか
+    private void OnTriggerEnter(Collider other)
+    {
+        if(_isDelivered)return;
+
+        if (other.CompareTag("DeliveryLocation"))
+        {
+            _isDelivered = true;
+
+            //スコアマネージャーにスコアを加算
+            var scoreManager = Object.FindAnyObjectByType<ScoreManager>();
+            if (scoreManager != null)
+            {
+                scoreManager.AddScore(_luggagescore);
+            }
+            
+            //荷物を削除
+            Destroy(gameObject);
+        }
+    }
+
 
 }
