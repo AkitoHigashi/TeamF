24e8054398ba651cb14a4ad12feadc921aea8f78
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-01 00:53:39 +0900

    <update>キー入力をここで取得

--- Assets/Z_Miyamoto/Scripts/Player/GetItem.cs
+++ Assets/Z_Miyamoto/Scripts/Player/GetItem.cs
@@ -2,14 +2,50 @@ using UnityEngine;
 
 public class GetItem : MonoBehaviour
 {
-    public Inventory Inventory;
+    public Inventory2 Inventory;
+    private ItemInfo _currentItem;
     private void OnTriggerEnter(Collider other)
     {
         var item = other.GetComponent<ItemInfo>();
         if (item)
         {
-            Inventory.AddItem(item.Item, 1);
-            Destroy(other.gameObject);
+            _currentItem = item;
+            Debug.Log("キーを押してインベントリに格納してください");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        var item = other.GetComponent<ItemInfo>();
+        if (item)
+        {
+            _currentItem = null;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && _currentItem != null)
+        {
+            if (Inventory.InputToSlot(_currentItem.Item, 0))
+            {
+                Destroy(_currentItem.gameObject);
+                _currentItem = null;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && _currentItem != null)
+        {
+            if (Inventory.InputToSlot(_currentItem.Item, 1))
+            {
+                Destroy(_currentItem.gameObject);
+                _currentItem = null;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && _currentItem != null)
+        {
+            if (Inventory.InputToSlot(_currentItem.Item, 2))
+            {
+                Destroy(_currentItem.gameObject);
+                _currentItem = null;
+            }
         }
     }
 }
