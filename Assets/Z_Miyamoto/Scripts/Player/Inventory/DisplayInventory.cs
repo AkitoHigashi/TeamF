199ef31813cdf80c9834fd78256aa8e915e12852
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-01 01:02:24 +0900

    <add>UIでアイコンを表示するためのスクリプト(途中)

---
+++ Assets/Z_Miyamoto/Scripts/Player/Inventory/DisplayInventory.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public Inventory2 Inventory;
+    public Image [] SlotImages;
+
+    public void UpdateUI()
+    {
+        
+    }
+}
