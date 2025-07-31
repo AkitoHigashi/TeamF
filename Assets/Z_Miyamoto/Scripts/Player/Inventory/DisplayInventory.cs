87ff068ac7375a92bf03655ed171d48c139f3a41
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-08-01 01:55:09 +0900

    <update>インベントリスロットの番号ごとに入っているアイテムのデータを参照してUIに表示できるようにした

--- Assets/Z_Miyamoto/Scripts/Player/Inventory/DisplayInventory.cs
+++ Assets/Z_Miyamoto/Scripts/Player/Inventory/DisplayInventory.cs
@@ -6,8 +6,23 @@ public class DisplayInventory : MonoBehaviour
     public Inventory2 Inventory;
     public Image [] SlotImages;
 
+    /// <summary>
+    /// スロットに入っているデータを参照してUIに表示するためのメソッド
+    /// </summary>
     public void UpdateUI()
     {
-        
+        for (int i = 0; i < SlotImages.Length; i++)
+        {
+            if (Inventory.Container.Count > i && Inventory.Container[i] != null && Inventory.Container[i].Item != null)
+            {
+                SlotImages[i].sprite = Inventory.Container[i].Item.Icon;
+                SlotImages[i].enabled = true;
+            }
+            else
+            {
+                SlotImages[i].sprite = null;
+                SlotImages[i].enabled = false;
+            }
+        }
     }
 }
