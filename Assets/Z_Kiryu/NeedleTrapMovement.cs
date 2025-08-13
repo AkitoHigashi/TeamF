a1e1e01402e9c579b780c310ccb952e01ad0ba1a
KINOKO\nubob <Ki28ryu@icloud.com>
2025-08-14 07:01:12 +0900

    [cleen]ファイルを何個か送るのを忘れてた

---
+++ Assets/Z_Kiryu/NeedleTrapMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NeedleTrapMovement : MonoBehaviour
+{
+    [SerializeField, Tooltip("トラップの待ち時間")]
+    private float WaitTime = 1f;
+
+    [SerializeField, Tooltip("トラップの上がり幅")]
+    private float NeedlY = 1f;
+
+    [SerializeField,Tooltip("移動にかかる時間")]
+    private float MoveDuration = 0.5f;
+
+    private Vector3 _startPos;
+    private Vector3 _endPos;
+
+    void Start()
+    {
+        //初めの位置を保存
+        _startPos = transform.position;
+        _endPos = _startPos + new Vector3(0, NeedlY, 0);
+
+        StartCoroutine(MoveTrapLoop());
+    }
+
+    IEnumerator MoveTrapLoop()
+    {
+
+
+        while (gameObject)
+        {
+            // 上がる
+            yield return StartCoroutine(SmoothMove(_startPos, _endPos, MoveDuration));
+            yield return new WaitForSeconds(WaitTime);
+
+            // 下がる
+            yield return StartCoroutine(SmoothMove(_endPos, _startPos, MoveDuration));
+            yield return new WaitForSeconds(WaitTime);
+        }
+    }
+
+    IEnumerator SmoothMove(Vector3 from, Vector3 to, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            transform.position = Vector3.Lerp(from, to, t);
+            yield return null; // 次のフレームまで待つ
+        }
+
+        transform.position = to; // 最後に位置をしっかり合わせる
+    }
+}
---
+++ Assets/Z_Kiryu/TrapTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TrapTrigger : MonoBehaviour
+{
+    public float KnockbackForce = 10f;
+    private Rigidbody _rb;
+    private const string TRAP_TAG_NAME = "NeedleTrap";
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+        if (!TryGetComponent(out _rb))
+        {
+            Debug.LogError($"Rigidbody of {gameObject.name} is null");
+            return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TRAP_TAG_NAME))
+        {
+            Debug.Log("敵と衝突した");
+            //ノックバック方向の計算
+            Vector3 knockbackDirection = (transform.position - other.transform.position).normalized;
+
+            _rb.AddForce(knockbackDirection * KnockbackForce, ForceMode.Impulse);
+        }
+    }
+}
